47999ee96504ed4a22aae51218753bf775e51387
animesh1414 <e22cseu1414@bennett.edu.in>
2024-08-31 00:41:51 +0530

    This commit contains the latest development in the homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import "./App.css";
+import React from 'react';
+//import Navbar from './components/Navbar';
+import Home1 from './components/Home1';
+//import Home2 from './components/Home2';
 
 function App() {
   return (
     <div>
-      hello
+      <div className='h-[100vh] w-[100vw]'>
+        <Home1/>
+      </div>
+      {/*<div className='h-[100vh] w-[100vw]'>
+        <Home2/>
+      </div>*/}
     </div>
   );
 }
